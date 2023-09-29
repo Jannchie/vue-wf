65cc1ec29bf025a3055f5c7e3d23911aae0b7f64
Jianqi Pan <jannchie@gmail.com>
2023-09-30 02:09:20 +0900

    :adhesive_bandage: fix(build): include workspace root

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import UnoCSS from 'unocss/vite'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), UnoCSS(), dts({
+  plugins: [vue(), vueJsx(), dts({
     include: './src',
     insertTypesEntry: true,
     cleanVueFileName: true,
